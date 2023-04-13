c4318d85c90a0ad7d4499633ecd8029c712a028f
KThomson11 <1351561863@qq.com>
2023-04-12 19:11:39 -0700

    forth testing

---
+++ .github/workflows/Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:18.10
+
+RUN echo "Success to build... Hello World..."
+
+WORKDIR /app
+
+COPY . .
+
+CMD ["echo","Hello World..."]
+
+
---
+++ .github/workflows/build-hello-image.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+IMAGE_NAME="my-hello-world"
+IMAGE_TAG="latest"
+
+docker build -t $IMAGE_NAME:$IMAGE_TAG .
+docker push $IMAGE_NAME:$IMAGE_TAG
+docker run $IMAGE_NAME
